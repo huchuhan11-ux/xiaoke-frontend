0e3ae7525c54abb2e3f8afd8f41eed25d6228349
hhh <hhh@MacBook-Neo.local>
2026-06-10 13:54:40 +0800

    add poke and countdown

---
+++ public/sw.js
@@ -0,0 +1,22 @@
+const CACHE = 'ourplace-v1'
+const ASSETS = ['/', '/index.html']
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  )
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', e => {
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return
+  e.respondWith(
+    fetch(e.request).catch(() =>
+      caches.match(e.request)
+    )
+  )
+})
